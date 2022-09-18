e7398fe94656d593e07cc7b397f38a6d2bf2a6f0
OBNinjaa <64389208+OBNinjaa@users.noreply.github.com>
2022-09-18 16:43:02 +0100

    death event

---
+++ src/events/mineflayer/deathEvent.js
@@ -0,0 +1,62 @@
+const mineflayer = require("mineflayer");
+const colors = require("colors");
+const request = require("request");
+
+const { messagehook, host } = require("../../config.json");
+
+/**
+ * @param {mineflayer.Bot} bot
+ */
+
+module.exports = (bot) => {
+  bot.on("messagestr", (message, messagePosition, jsonMsg) => {
+    if (jsonMsg.json.translate === "death.attack.player") {
+      console.log(`[${new Date().toLocaleTimeString().gray}] ${message.magenta}`);
+      var URL = messagehook;
+      request.post(URL, {
+        json: {
+          content: null,
+          embeds: [
+            {
+              title: `**${jsonMsg.json.with[0].insertion} Killed**`,
+              description: `**${message}**`,
+              color: 16276813,
+              footer: {
+                text: `${new Date().toLocaleTimeString()}`,
+              },
+              thumbnail: {
+                url: `https://mc-heads.net/head/${jsonMsg.json.with[0].insertion}`,
+              },
+            },
+          ],
+          attachments: [],
+        },
+      });
+    }
+
+    // Admin Kill
+    if (jsonMsg.json.translate === "chat.type.admin") {
+      console.log(`[${new Date().toLocaleTimeString().gray}] ${message.magenta}`);
+      var URL = messagehook;
+      request.post(URL, {
+        json: {
+          content: null,
+          embeds: [
+            {
+              title: `**OP Killed ${jsonMsg.json.with[0].insertion}**`,
+              description: `**${message}**`,
+              color: 16276813,
+              footer: {
+                text: `${new Date().toLocaleTimeString()}`,
+              },
+              thumbnail: {
+                url: `https://mc-heads.net/head/${jsonMsg.json.with[0].insertion}`,
+              },
+            },
+          ],
+          attachments: [],
+        },
+      });
+    }
+  });
+};
