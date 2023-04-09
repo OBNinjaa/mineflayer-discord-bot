28897d9b24025e6f16a2a9189e84f8b60d19ac49
OBNinjaa <64389208+OBNinjaa@users.noreply.github.com>
2023-04-10 00:05:54 +0100

    spam reduction

--- src/modules/message.js
+++ src/modules/message.js
@@ -10,10 +10,21 @@ const { webhook } = options;
  */
 
 module.exports = (bot) => {
+  const spam = {};
+  const spamTimeout = 5000;
+
   bot.on("chat", (username, message) => {
     if (username === bot.username) return;
     if (username === "chat") username = "Discord";
     if (message.includes("http")) return;
+
+    const now = Date.now();
+    if (spam[username] && now - spam[username] < spamTimeout) {
+      console.log(`[${new Date().toLocaleTimeString().gray}] [${username.brightBlue}] ${message.brightYellow} (ignored due to spam)`);
+      return;
+    }
+    spam[username] = now;
+
     console.log(`[${new Date().toLocaleTimeString().gray}] [${username.brightBlue}] ${message.brightYellow}`);
 
     var URL = `${webhook}`;
