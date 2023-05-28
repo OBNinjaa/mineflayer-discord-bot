c512435eb3e3f88f530c4597856f480482634b92
OBNinjaa <64389208+OBNinjaa@users.noreply.github.com>
2023-05-28 10:18:22 +0100

    auto mine + auto farm

--- src/commands/mine.js
+++ src/commands/mine.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const bot = require("../bot");
+const collectBlock = require("mineflayer-collectblock").plugin;
+const mcData = require("minecraft-data")(bot.version);
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -7,19 +9,55 @@ module.exports = {
     .setDescription("Have the bot mine any mineable block in the game.")
     .addStringOption((option) => option.setName("block").setDescription("The block to mine e.g diamond_ore")),
   async execute(interaction) {
-    const value = interaction.options.getString("block");
+    try {
+      const value = interaction.options.getString("block");
 
-    const head = "https://api.mineatar.io/head/" + value;
+      if (!value) return interaction.reply("You did not input a block name!");
 
-    const mineEmbed = new EmbedBuilder()
-      .setColor(0xf58367)
-      .setTitle(`Mining: ${value}`)
-      .setThumbnail(head)
-      .setDescription(`**${bot.username}** is now mining: ${value}`)
-      .setTimestamp();
+      const head = "https://api.mineatar.io/head/" + value;
 
-    if (value) return interaction.reply({ embeds: [mineEmbed] });
+      const mineEmbed = new EmbedBuilder()
+        .setColor(0xf58367)
+        .setTitle(`Mining: ${value}`)
+        .setThumbnail(head)
+        .setDescription(`**${bot.username}** is now mining: ${value}`)
+        .setTimestamp();
 
-    return interaction.reply("You did not input a block name!");
+      await interaction.reply({ embeds: [mineEmbed] });
+
+      bot.loadPlugin(require("mineflayer-collectblock").plugin);
+
+      const blockType = mcData.blocksByName[value];
+
+      if (!blockType) {
+        console.log(`I don't know any blocks named ${value}.`);
+        return;
+      }
+
+      const block = bot.findBlock({
+        matching: blockType.id,
+        maxDistance: 64,
+      });
+
+      if (!block) {
+        console.log(`Cannot find ${value} nearby.`);
+        return;
+      }
+
+      console.log(block);
+
+      bot.collectBlock.collect(block, (err) => {
+        if (err) {
+          console.log(`Error while mining ${value}: ${err}`);
+          return interaction.followUp(`Error while mining ${value}: ${err}`);
+        }
+
+        console.log(`Successfully mined ${value}.`);
+        interaction.followUp(`Successfully mined ${value}.`);
+      });
+    } catch (error) {
+      console.error(error);
+      interaction.followUp("There was an error while executing this command!");
+    }
   },
 };
