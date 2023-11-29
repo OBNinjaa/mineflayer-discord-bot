e7348f0daba8e94ce9ac7de62573a8aa0014bed5
OBNinjaa <64389208+OBNinjaa@users.noreply.github.com>
2023-11-29 19:27:54 +0000

    retry count

--- src/modules/message.js
+++ src/modules/message.js
@@ -7,6 +7,7 @@ const { messages } = require("../settings.json");
  */
 
 module.exports = (bot) => {
+  let retry = 0;
   bot.on("chat", (username, message) => {
     if (!message.includes("https")) {
       console.log(`\x1b[33m${username} : \x1b[35m${message}\x1b[0m`);
@@ -19,8 +20,10 @@ module.exports = (bot) => {
           },
         });
       } catch (error) {
+        if (retry === 3) return;
         console.log("\x1b[31mUnable to send webhook to Discord\x1b[0m");
         console.log("\x1b[31mMake sure you have a valid webhook in your config \x1b[0m");
+        retry++;
       }
     }
   });
