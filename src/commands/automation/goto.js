d7afd0bd8ebc1861080ae1ba6ac2042d47bb101f
OBNinjaa <64389208+OBNinjaa@users.noreply.github.com>
2023-11-29 19:27:29 +0000

    no path

--- src/commands/automation/goto.js
+++ src/commands/automation/goto.js
@@ -17,6 +17,11 @@ module.exports = {
     const y = interaction.options.getString("y");
     const z = interaction.options.getString("z");
 
+    const position = bot.entity.position;
+    const roundedX = Math.floor(position.x);
+    const roundedY = Math.floor(position.y);
+    const roundedZ = Math.floor(position.z);
+
     const initialize = new Embed(bot).setTitle("Pathfinding Has Initialized").setDescription(`pathfinding to the specified coordinates: \`${x} ${y} ${z}\`\nYou will be notified when the path has been reached.`);
     await interaction.reply({ embeds: [initialize] });
 
@@ -24,12 +29,14 @@ module.exports = {
     bot.pathfinder.setMovements(defaultMove);
     bot.pathfinder.setGoal(new GoalNear(x, y, z, 1));
 
-    bot.once("goal_reached", () => {
-      const position = bot.entity.position;
-      const roundedX = Math.floor(position.x);
-      const roundedY = Math.floor(position.y);
-      const roundedZ = Math.floor(position.z);
+    bot.on("path_update", (path) => {
+      if (path.status === "noPath") {
+        const noPath = new Embed(bot).setTitle("Unable To Reach Path").setDescription(`The bot is unable to reach the path: \`${roundedX}, ${roundedY}, ${roundedZ}\`\nMake sure the bot has enough meterial to build!`);
+        return interaction.editReply({ embeds: [noPath] });
+      }
+    });
 
+    bot.once("goal_reached", () => {
       const stopped = new Embed(bot).setTitle("Pathfinding Has Stopped").setDescription(`The coordinates have been reached. The bots current path is as follows: \`${roundedX}, ${roundedY}, ${roundedZ}\``);
       return interaction.followUp({ embeds: [stopped] });
     });
